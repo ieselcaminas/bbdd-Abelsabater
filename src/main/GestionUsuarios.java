3d847905cc3101f90e81f80c8da276d8bac5d166
Abelsabater <abesabmun@alu.edu.gva.es>
2025-02-14 13:29:43 +0100

    Base de Datos Abel Sabater

--- src/main/GestionUsuarios.java
+++ src/main/GestionUsuarios.java
@@ -1,3 +1,4 @@
+import java.sql.*;
 import java.util.Scanner;
 
 public class GestionUsuarios {
@@ -10,8 +11,31 @@ public class GestionUsuarios {
             System.out.println("3 -Comentarios");
             opcion=sc.nextInt();
             if(opcion== 1){
-                GestionUsuarios.gestionMenu();
+
             }
         }
+    }
+    public static boolean existeUsuario() throws SQLException {
+        java.sql.Connection con = Main.connection;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Ingrese su usuario");
+        String usuario=sc.nextLine();
+        System.out.println("Ingrese su password");
+        String password=sc.nextLine();
+        PreparedStatement st=con.prepareStatement("select * from usuarios where usuario=? and password=?");
+        st.setString(1, usuario);
+        st.setString(2, password);
+        ResultSet rs = st.executeQuery();
+        return rs.next();
+    }
+    public static void crearUsuario() throws SQLException {
+        java.sql.Connection con = Main.connection;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Ingrese su usuario");
+        String usuario=sc.nextLine();
+        System.out.println("Ingrese su password");
+        String password=sc.nextLine();
+        Statement st = con.createStatement();
+        st.executeUpdate("INSERT INTO usuarios (nombre, contrasenya) VALUES(usuario,password)");
     }
 }
