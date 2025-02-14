99c6371aee50a698a2f5120de97b81b75e04fcc6
Abelsabater <abesabmun@alu.edu.gva.es>
2025-02-14 12:54:03 +0100

    Base de Datos Abel Sabater

--- src/main/GestionUsuarios.java
+++ src/main/GestionUsuarios.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class GestionUsuarios {
     public static void gestionMenu(){
-
+        Scanner sc=new Scanner(System.in);
+        int opcion=0;
+        while (opcion !=-1){
+            System.out.println("1 -Usuarios");
+            System.out.println("2 -Posts");
+            System.out.println("3 -Comentarios");
+            if(opcion== 1){
+                GestionUsuarios.gestionMenu();
+            }
+        }
     }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -2,15 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int opcion=0;
-        while (opcion !=-1){
-            System.out.println("1 -Usuarios");
-            System.out.println("2 -Posts");
-            System.out.println("3 -Comentarios");
-            if(opcion== 1){
-                GestionUsuarios.gestionMenu();
-            }
-        }
+
     }
 }
