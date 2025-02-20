627d9759b2180df08b77d5eaacefc6d797c98993
Abelsabater <abesabmun@alu.edu.gva.es>
2025-02-20 14:05:20 +0100

    Base de Datos Abel Sabater

--- src/main/GestionPost.java
+++ src/main/GestionPost.java
@@ -1,2 +1,28 @@
+import java.sql.SQLException;
+import java.util.Date;
+import java.util.Scanner;
+
 public class GestionPost {
+    public static void gestionMenu() throws SQLException {
+        Scanner sc = new Scanner(System.in);
+        String usuario;
+        int opcion = 0;
+        while (opcion != -1) {
+            System.out.print("1-Crear post ");
+            System.out.println("-1-Salir");
+            if (sc.nextInt() == 1) {
+                nuevopost();
+            }
+        }
+    }
+    public static void nuevopost() throws SQLException {
+        Scanner sc = new Scanner(System.in);
+        String texto;
+        int likes;
+        int id_usuario;
+        java.sql.Date fecha = new java.sql.Date(new Date().getTime());
+        System.out.println("Que vols publicar?");
+        texto = sc.nextLine();
+    }
+
 }
